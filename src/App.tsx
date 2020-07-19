c302edf7ebe38c308c4cb3f169c26b2ac078b7bf
Mk6rqD56HfK9Ps6 <lukzerom@gmail.com>
2020-07-19 14:50:23 +0200

    First question appears

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Difficulty, fetchQuizQuestions, QuestionsState } from "./API";
+import QuestionCard from "./components/QuestionCard";
+const TOTAL_QUESTIONS = 10;
 
-function App() {
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionsState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>();
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  useEffect(() => {
+    console.log(questions);
+  });
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const nextQuestion = () => {};
   return (
     <div className="App">
-      <h1>My app</h1>
+      <h1>React quiz</h1>
+      {gameOver || userAnswers?.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+
+      {!gameOver ? <p className="score">Score:</p> : null}
+      {loading ? <p>Loading Questions...</p> : null}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+
+      <button className="next" onClick={nextQuestion}>
+        Next question
+      </button>
     </div>
   );
-}
+};
 
 export default App;
